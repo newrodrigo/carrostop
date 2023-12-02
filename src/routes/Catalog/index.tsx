b1fd84c8b1ea997b6c19607705b30afc34b83600
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-01 21:16:10 -0300

    search bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,5 +1,21 @@
+import "./styles.css";
+
 import Header from "../../components/Header";
+import SearchBar from "../../components/SearchBar";
 
 export default function Catalog() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section" className="ct-container">
+          <div className="ct-search-bar-container">
+           <SearchBar />
+          </div>
+          <div className="ct-catalog-cards-container">
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
